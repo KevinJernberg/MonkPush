2efe3c14400ad63f7ad4de76ef804a2ce7c07a2d
KevinJernberg <a22kevje@student.his.se>
2022-11-28 02:20:27 +0100

    Gjorde det sista på Pushable objects
    Speltesta gärna. Röda block kan man putta, svarta kan man inte putta.
    
    Jag har gjort så att blocken inte kan gå igenom varandra. Lagt dit en tag för objekt som blockerar pushables.

--- Assets/Scripts/RaycastPlayerHit.cs
+++ Assets/Scripts/RaycastPlayerHit.cs
@@ -1,3 +1,4 @@
+using System;
 using StarterAssets;
 using UnityEngine;
 
@@ -17,6 +18,9 @@ public class RaycastPlayerHit : MonoBehaviour
     private Vector3 _oldPos;
     private Vector3 _newMovedPos;
 
+    private Vector3 _boxForwardDirection;
+
+
     private enum PushDir
     {
         West,
@@ -26,8 +30,6 @@ public class RaycastPlayerHit : MonoBehaviour
     }
 
     private PushDir _pushedDirection;
-    
-    // Update is called once per frame
 
     private void Start()
     {
@@ -36,11 +38,11 @@ public class RaycastPlayerHit : MonoBehaviour
 
     void Update()
     {
+        // Character push check ray
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 1f;
-        var pushRay = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-        
+        Vector3 pushRay = transform.position + new Vector3(0, 1, 0);
         Debug.DrawRay(pushRay, forward, Color.green);
-        
+
         if (Physics.Raycast(pushRay, transform.TransformDirection(Vector3.forward), out _hitBlock, 1f, 64))
         {
             _foundHitBlock = _hitBlock;
@@ -51,36 +53,43 @@ public class RaycastPlayerHit : MonoBehaviour
                     _input.push = false;
                     _oldPos = _foundHitBlock.transform.position;
                     
-                    float characterDiagonalCheck = transform.rotation.eulerAngles.y;
-
-                    if (characterDiagonalCheck is < 30 or > 330) //Pos Z
+                    float characterDiagonalCheck = transform.rotation.eulerAngles.y; // Use Angle instead of radians
+                    
+                    
+                    //TODO: Make this a switch case, to increase readability 
+                    if (characterDiagonalCheck is < 30 or > 330) //Pos Z 
                     {
                         _pushedDirection = PushDir.North;
                         _newMovedPos = _foundHitBlock.transform.position + new Vector3(0, 0, 2);
+                        _boxForwardDirection = new Vector3(0, 0, 1);
+
                     }
                     else if (characterDiagonalCheck is < 120 and > 60) //Neg X
                     {
                         _pushedDirection = PushDir.West;
-                        
                         _newMovedPos = _foundHitBlock.transform.position + new Vector3(2, 0, 0);
-                        //_hitBlock.transform.position = Vector3.Lerp(_hitBlock.transform.position,
-                            //_hitBlock.transform.position + new Vector3(2, 0, 0), 1);
+                        _boxForwardDirection = new Vector3(1, 0, 0);
                     }
                     else if (characterDiagonalCheck is < 210 and > 150) //Neg Z
                     {
                         _pushedDirection = PushDir.South;
                         _newMovedPos = _foundHitBlock.transform.position + new Vector3(0, 0, -2);
-                        //_hitBlock.transform.position = Vector3.Lerp(_hitBlock.transform.position,
-                            //_hitBlock.transform.position + new Vector3(0, 0, -2), 1);
+                        _boxForwardDirection = new Vector3(0, 0, -1);
                     }
                     else if (characterDiagonalCheck is < 300 and > 240) //Pos X
                     {
                         _pushedDirection = PushDir.East;
                         _newMovedPos = _foundHitBlock.transform.position + new Vector3(-2, 0, 0);
-                        //_hitBlock.transform.position = Vector3.Lerp(_hitBlock.transform.position,
-                            //_hitBlock.transform.position + new Vector3(-2, 0, 0), 1);
+                        _boxForwardDirection = new Vector3(-1, 0, 0);
                     }
-                    _pushTime = 0;
+                    
+                    // Sets a new position for the collision check. Right above the lowest point of the block.
+                    Vector3 collisionCheckOriginPoint = _oldPos - new Vector3(0, _foundHitBlock.transform.localScale.y * 0.5f - 0.1f, 0); 
+                    
+                    if (!Physics.Raycast(collisionCheckOriginPoint, _boxForwardDirection, 2f, 192))
+                    {
+                        _pushTime = 0; // Starts the push mechanism
+                    }
                 }
             }
         }
@@ -88,6 +97,7 @@ public class RaycastPlayerHit : MonoBehaviour
         {
             _input.push = false;
         }
+        
         if (_pushTime < 1)
         {
             _pushTime += _pushSpeed * Time.deltaTime;
@@ -98,6 +108,7 @@ public class RaycastPlayerHit : MonoBehaviour
             }
             switch (_pushedDirection)
             {
+                // Move the block to specific coordinate with relation to time 
                 case PushDir.North:
                     _foundHitBlock.transform.position = Vector3.Lerp(_oldPos, _newMovedPos, _pushTime);
                     break;
